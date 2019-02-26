90e730f7a328c3ce24a9641473f885bf74422e69
Adis Puangchompoo <apuangchompoo@gmail.com>
2019-02-26 14:05:17 +0900

    update, destroy added to controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,10 +1,11 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:show, :edit, :update, :delete]
+
   def index
     @dogs = Dog.all
   end
 
   def show
-    @dog = Dog.find(params[:id])
   end
 
   def new
@@ -23,7 +24,26 @@ class DogsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @dog.update(dog_params)
+      redirect_to dog_path(@dog)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dog.destroy
+  end
+
   private
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
 
   def dog_params
     params.require(:dog).permit(:name, :description, :breed, :image_url, :available_dates)
