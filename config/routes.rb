b91d42a0a2e9cefb878a7455a127acbac64f9d4d
Adis Puangchompoo <apuangchompoo@gmail.com>
2019-02-26 17:36:20 +0900

    Users controllers with view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :dogs, only: [:index, :show, :edit, :update, :delete]
+  resources :users, only: [:show]
   resources :owners, only: [:show] do
     resources :dogs, only: [:new, :create]
   end
