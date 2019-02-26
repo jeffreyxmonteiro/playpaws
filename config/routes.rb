480a22984a695a40a9355d20e784dfde167c6729
Adis Puangchompoo <apuangchompoo@gmail.com>
2019-02-26 17:55:55 +0900

    playdates controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :owners
   devise_for :users
   root to: 'pages#home'
-  resources :dogs, only: [:index, :show, :edit, :update, :delete]
+  resources :dogs, only: [:index, :show, :edit, :update, :delete] do
+    resources :playdates, only: [:new, :create]
+  end
   resources :users, only: [:show]
   resources :owners, only: [:show] do
     resources :dogs, only: [:new, :create]
   end
   resource :profile, only: [:show, :edit, :update]
+  resources :playdates, only: [:index, :show]
 end
