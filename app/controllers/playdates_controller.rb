0a51002105b04ec0fc3fabcf7c561ce0c7dd958d
Adis Puangchompoo <apuangchompoo@gmail.com>
2019-02-27 13:44:06 +0900

    users can now add a playdate + redirect to profile

--- app/controllers/playdates_controller.rb
+++ app/controllers/playdates_controller.rb
@@ -22,11 +22,12 @@ class PlaydatesController < ApplicationController
     @playdate.user = current_user
     @playdate.dog = @dog
     if @playdate.save
-      redirect_to playdate_path(@playdate)
+      # remove available dates, move this to confirmation page when complete
+      @dog.available_dates.delete(@playdate.date)
+      redirect_to profile_user_path
     else
       render :new
     end
-    skip_authorization
   end
 
   private
